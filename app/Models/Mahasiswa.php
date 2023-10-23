a066b4edac7f1eb99118e52757158c68eabefcce
dhiyaindah <dhiyaindah@yahoo.com>
2023-10-23 10:59:25 +0700

    model migration mhs

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'mahasiswa';
+    protected $primaryKey = 'nim';
+    public $incrementing = false;
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nim',
+        'nama',
+        'angkatan',
+        'status',
+        'jalur_masuk',
+        'alamat',
+        'kabupaten',
+        'provinsi',
+        'email',
+        'nomor_telepon',
+    ];
+}
