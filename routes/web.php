88945e78046e791c8cd5a64721b3bff1efb3169a
yusufnajhan <najhan.icp@gmail.com>
2023-10-25 11:36:22 +0700

    menambahkan use pada route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
