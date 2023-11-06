ff9b77dd353fec0991b146dddcf9e3f75f9639ec
yusufnajhan <najhan.icp@gmail.com>
2023-11-06 10:23:56 +0700

    memperjelas pesan error di halaman login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -24,7 +24,7 @@
                         <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-100 dark:bg-gray-800 dark:text-red-400"
                             role="alert">
                             <span class="font-medium">Log-In Gagal!</span>
-                            <p>Silakan Coba Kembali.</p>
+                            <p>{{ session('loginError') }}</p>
                         </div>
                     @endif
 
