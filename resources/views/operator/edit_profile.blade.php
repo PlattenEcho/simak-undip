4f579696475795c30323b7347d11e4b958bd3011
lailynovita <lailynovita29@gmail.com>
2023-11-06 11:39:08 +0700

    edit profil operator di navsidebar

--- resources/views/operator/edit_profile.blade.php
+++ resources/views/operator/edit_profile.blade.php
@@ -1,4 +1,4 @@
-@extends('mahasiswa.navsidebar')
+@extends('operator.navsidebar')
 
 @section('content')
     <div class="p-4 sm:ml-64">
@@ -23,7 +23,7 @@
                 Profil Operator
             </h1>
 
-            <form enctype="multipart/form-data" class="space-y-4 md:space-y-6" method="POST" autocomplete="on" action="{{ route('mahasiswa.update') }}" >
+            <form enctype="multipart/form-data" class="space-y-4 md:space-y-6" method="POST" autocomplete="on" action="{{ route('operator.update') }}" >
             @csrf
             <div class="flex flex-col items-center mb-6">
                 <div class="relative w-20 h-20 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
@@ -88,7 +88,7 @@
             </div>
             <div class="form-group">
                 <label for="alamat" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Alamat</label>
-                <textarea id="alamat" name="alamat" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">{{ $mahasiswa->alamat }}</textarea>
+                <textarea id="alamat" name="alamat" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">{{ $operator->alamat }}</textarea>
                 @error('alamat')
                     <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-100 dark:bg-gray-800 dark:text-red-400"
                         role="alert">
@@ -115,7 +115,7 @@
                     class="mr-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                     Save
                 </button>
-                <a href="{{ route('mahasiswa.viewEditProfile') }}"
+                <a href="{{ route('operator.viewEditProfile') }}"
                     class="mr-auto text-white bg-gray-400 hover:bg-gray-500 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-400 dark:hover:bg-gray-500 focus:outline-none dark:focus:ring-gray-600">
                     Reset
                 </a>
