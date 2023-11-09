ec915661767750348c69034c9de8828f5c2fada0
lailynovita <lailynovita29@gmail.com>
2023-11-09 10:56:00 +0700

    profile doswal

--- resources/views/doswal/navsidebar.blade.php
+++ resources/views/doswal/navsidebar.blade.php
@@ -42,7 +42,7 @@
                                 </div>
                                 <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownInformdropdownAvatarNameButtonationButton">
                                     <li>
-                                        <a href="{{ route('mahasiswa.viewProfile') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Profile</a>
+                                        <a href="{{ route('doswal.viewProfile') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Profile</a>
                                     </li>
                                     <li>
                                         <a href="{{ route('account.viewChangePassword') }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Change Password</a>
