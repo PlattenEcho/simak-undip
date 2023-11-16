d4c25c061c63b3bf6c4c484ae23c3d689067a895
PlattenEcho <akunsekolahdika@gmail.com>
2023-11-16 12:38:42 +0700

    implement validasi jum sks sebelum entry skripsi

--- resources/views/mahasiswa/skripsi.blade.php
+++ resources/views/mahasiswa/skripsi.blade.php
@@ -13,8 +13,7 @@
             @endif
 
             @if (session('error'))
-                <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-100 dark:bg-gray-800 dark:text-red-400"
-                    role="alert">
+                <div class="p-4 text-sm text-red-800 rounded-lg bg-red-100 dark:bg-gray-800 dark:text-red-400" role="alert">
                     <span class="font-medium">{{ session('error') }}</span>
                 </div>
                 <br>
