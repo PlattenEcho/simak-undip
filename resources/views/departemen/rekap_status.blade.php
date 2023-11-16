816cc457026adf1307b33e4717e01b7e90fa4dad
yusufnajhan <najhan.icp@gmail.com>
2023-11-16 16:55:39 +0700

    set rekap_status href='#'

--- resources/views/departemen/rekap_status.blade.php
+++ resources/views/departemen/rekap_status.blade.php
@@ -97,7 +97,7 @@
                     </tbody>
                 </table>
                 <div class="p-3">        
-                    <a href="{{ route('departemen.cetakRekapStatus', [$mhsData[0]->angkatan]) }}" type="button" class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cetak PDF</a>
+                    <a href="#" type="button" class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cetak PDF</a>
                 </div>
             </div>
         </div>
