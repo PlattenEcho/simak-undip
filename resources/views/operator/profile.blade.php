312281183f177424ed0fd98bc7301df08fef1639
lailynovita <lailynovita29@gmail.com>
2023-11-21 12:23:00 +0700

    tanpa scroll semua

--- resources/views/operator/profile.blade.php
+++ resources/views/operator/profile.blade.php
@@ -28,6 +28,7 @@
                     <img src="{{ Auth::user()->getImageURL() }}" alt="user photo" class="w-20 h-20 object-cover" />
                 </div>
             </div>
+            <div class="grid md:grid-cols-2 md:gap-6">
             <div class="form-group">
                 <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
                 <input type="text" id="username" name="username" value="{{ Auth::user()->username }}" aria-label="disabled input" class="mb-6 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" disabled>
@@ -54,9 +55,10 @@
             </div>
             <div class="m-auto">
                 <button type="submit" name="submit"
-                    class="mr-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                    class="mr-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ">
                     Edit Profil
                 </button>
+            </div>
             </div>
         </form>
     </div>
