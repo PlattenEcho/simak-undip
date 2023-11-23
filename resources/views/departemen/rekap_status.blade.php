4cc77f0a96e42926a5cf82959bfba26d1ff667ef
lailynovita <lailynovita29@gmail.com>
2023-11-23 12:56:50 +0700

    cetak pdf hapus dept

--- resources/views/departemen/rekap_status.blade.php
+++ resources/views/departemen/rekap_status.blade.php
@@ -119,9 +119,6 @@
                         @endforeach
                     </tr>
                 </table>
-                <div class="p-3">        
-                    <a href="#" type="button" class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cetak PDF</a>
-                </div>
             </div>
         </div>
     </div>
